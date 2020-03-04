0c1b8c4fcffd99b5f44f80de6892e49fafa29546
Ramaraja <ramaraja.rm@gmail.com>
2020-03-04 23:18:02 +0530

    converting tabs to spaces

--- random_parser.py
+++ random_parser.py
@@ -1,27 +1,27 @@
-import re
+# parser
 
 data = """
 NAME: "1", "DESCR": "C1000"
-PID: EC1K-TEST			, VID: V00  ,  SN: 193823648
+PID: EC1K-TEST          , VID: V00  ,  SN: 193823648
 OID: 1.2.3.4.5.6
 
 NAME: "2", "DESCR": "C1000"
-PID: EC1K-TEST			, VID: V00  ,  SN: 193823648
+PID: EC1K-TEST          , VID: V00  ,  SN: 193823648
 OID: 1.2.3.4.5.6
 """
 
 def oid_parser(data):
-	# assumed that the double newlines are seperate records
-	records = data.split('\n\n')
-	result = [] # to store the final result
-	for item in records:
-		# remove space, newline, tabs and flatterning
-		flat_val = item.strip(' \n\t\"').replace("\n",",").replace('"', '')
-		# still some spaces?? one more strip - Need to find a better way to do this.
-		rec = [x.strip() for x in flat_val.split(',')]
-		# convert to dict and store
-		result.append(dict(r.strip(' ').split(':') for r in rec))
-	return result
+    # assumed that the double newlines are seperate records
+    records = data.split('\n\n')
+    result = [] # to store the final result
+    for item in records:
+        # remove space, newline, tabs and flatterning
+        flat_val = item.strip(' \n\t\"').replace("\n",",").replace('"', '')
+        # still some spaces?? one more strip - Need to find a better way to do this.
+        rec = [x.strip() for x in flat_val.split(',')]
+        # convert to dict and store
+        result.append(dict(r.strip(' ').split(':') for r in rec))
+    return result
 
 
 res = oid_parser(data)
@@ -30,5 +30,5 @@ print(res)
 # test searching name and fetch oid
 
 for item in res:
-	if str(item.get("NAME")).strip() == "1":
-		print(item.get("OID"))
+    if str(item.get("NAME")).strip() == "1":
+        print(item.get("OID"))
